9ac6c1fdc90e9cac534bdd6097509575cb55c1a6
MAC <mac@Mucus-MacBook-Pro.local>
2026-03-05 04:28:24 +0300

    add deploy workflow

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress';
 import { viteBundler } from '@vuepress/bundler-vite';
 
 export default defineUserConfig({
+  base: '/ubunifu/',
   lang: 'en-US',
   title: 'UBUNIFU SACCO Documentation',
   description: 'Comprehensive documentation for the UBUNIFU SACCO project',
